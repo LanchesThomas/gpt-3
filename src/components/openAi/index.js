45f5011d2472015c10d658f7b0465d3f8a826259
Sotham <lanches.thomas@gmail.com>
2023-02-03 17:14:26 +0100

    add filter on openai

--- src/components/openAi/index.js
+++ src/components/openAi/index.js
@@ -8,6 +8,7 @@ const OpenAiDiv = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 0px;
+    position: relative;
 
     #item-0 {
         grid-column-start: 2;
@@ -22,12 +23,12 @@ const OpenAiDiv = styled.div`
 `
 
 const TitleDiv = styled.div`
-display: flex;
-flex-direction: column;
-gap: 50px;
-@media (max-width: ${resolution.large}) {
-    gap: 15px;
-}
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    @media (max-width: ${resolution.large}) {
+        gap: 15px;
+    }
 `
 
 const Title = styled.h2`
@@ -46,10 +47,9 @@ const Request = styled.p`
 
 const SubContentDiv = styled.div`
     display: flex;
-    @media (max-width: ${resolution.large})  {
+    @media (max-width: ${resolution.large}) {
         gap: 30px;
     }
-
 `
 
 const SubTitleDiv = styled.div`
@@ -60,11 +60,9 @@ const SubTitleDiv = styled.div`
     align-items: flex-end;
     @media (max-width: ${resolution.large}) {
         align-items: flex-start;
-    justify-content: space-around;
-        
+        justify-content: space-around;
     }
-    `
-
+`
 
 const SubTitle = styled.p`
     width: 45%;
@@ -74,19 +72,18 @@ const SubTitle = styled.p`
     margin: 0px;
     padding-top: -10px;
 
-&:before {
-    display: block;
-    content: '';
-    margin-bottom: 10px;
-    width: 38px;
-    height: 3px;
-    background: linear-gradient(#AE67FA, #F49867);
-}
+    &:before {
+        display: block;
+        content: '';
+        margin-bottom: 10px;
+        width: 38px;
+        height: 3px;
+        background: linear-gradient(#ae67fa, #f49867);
+    }
 
-@media (max-width: ${resolution.large}) {
-    width: 100%;
-}
-
+    @media (max-width: ${resolution.large}) {
+        width: 100%;
+    }
 `
 
 const ContentDiv = styled.div`
@@ -96,23 +93,35 @@ const ContentDiv = styled.div`
     gap: 25px;
     justify-content: space-between;
     align-items: center;
-
 `
 
 const Content = styled.p`
-width: 75%;
-color: #81AFDD;
-font-size: 14px;
-font-weight: 500;
-margin: 0px 5px;
-@media (max-width: ${resolution.large}) {
-    width: 100%;
-}
+    width: 75%;
+    color: #81afdd;
+    font-size: 14px;
+    font-weight: 500;
+    margin: 0px 5px;
+    @media (max-width: ${resolution.large}) {
+        width: 100%;
+    }
+`
+
+const Filter = styled.div`
+    position: absolute;
+    border-radius: 50%;
+    width: 581px;
+    height: 410px;
+    z-index: -1;
+    background: #0f4279;
+    filter: blur(200px);
+    top: 0px;
+    left: -600px;
 `
 
 const OpenAi = () => {
     return (
         <OpenAiDiv id="open-ai">
+            <Filter />
             <TitleDiv>
                 <Title>
                     The Future is Now and You Just Need To Realize It. Step into
